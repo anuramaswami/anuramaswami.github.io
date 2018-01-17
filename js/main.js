b5e53ff2ad3ad8e746d3419014e97bed250cfafd
Rafi Barash <rafabarash@gmail.com>
2018-01-17 11:09:52 -0600

    active wasn't working...took it out

--- js/main.js
+++ js/main.js
@@ -1,14 +1,1 @@
-$(function () {
-  // this will get the full URL at the address bar
-  var url = window.location.href;
 
-  // passes on every "li" tag
-  $("nav li a").each(function () {
-    // checks if its the same on the address bar
-    if (url == (this.href)) {
-      $(this).addClass("active");
-      //for making parent of submenu active
-      $(this).parent().parent().addClass("active");
-    }
-  });
-});
